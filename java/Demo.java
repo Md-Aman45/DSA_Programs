bb59a0b05f64bd74425afedc7134de735cd25546
Md Aman <aman003245@gmail.com>
2025-09-23 01:56:10 +0530

    divisors

--- java/Demo.java
+++ java/Demo.java
@@ -1,26 +1,51 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
-// // RevNum + Palindrome...
+// // RevNum + Palindrome + Armstrong...
 public class Demo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int dup = n;
-        int count = 0;
-        int revNum = 0;
-        while(n > 0) {
-            int lastDigit = n % 10;
-            count++;
-            n = n / 10;
-            revNum = revNum * 10 + lastDigit;
-        }
-        if(dup == revNum) {
-            System.out.println("Is no. is palindrome:- Yes");
-        } else {
-            System.out.println("Is no. is palindrome:- No");
-        }
+        // int dup = n;
+        // int count = 0;
+        // int revNum = 0;
+        // int sum = 0;
+        // while(n > 0) {
+        //     int lastDigit = n % 10;
+        //     count++;
+        //     n = n / 10;
+        //     revNum = revNum * 10 + lastDigit;
 
-        System.out.println("No.:- " + count);
-        System.out.print("RevNum:- " + revNum);
+        //     sum = sum + (lastDigit  * lastDigit * lastDigit);
+        // }
+        // if(dup == revNum) {
+        //     System.out.println("Is no. is palindrome:- Yes");
+        // } else {
+        //     System.out.println("Is no. is palindrome:- No");
+        // }
+
+        // if(dup == sum) {
+        //     System.out.println("Is no. is armstrong:- Yes");
+        // } else {
+        //     System.out.println("Is no. is armstrong:- No");
+        // }
+
+        // System.out.println("No.:- " + count);
+        // System.out.print("RevNum:- " + revNum);
+        
+    // print All Divisions
+    ArrayList<Integer> divisors = new ArrayList<>();
+
+    for(int i = 1; i <= Math.sqrt(n); i++) {
+        if(n%i == 0) {
+            divisors.add(i);
+            if(n/i != i) {
+                divisors.add(n/i);
+            }
+        }
+    }
+    Collections.sort(divisors);
+    System.out.println("Numbers Divided by " + (n) + ":-" + (divisors));
     }
 }
--- python/demo.py
+++ python/demo.py
@@ -2,21 +2,41 @@
 # n = int(input())
 # print(n * n)
 
-# # Reverse Num + Palindrome...
-revNum = 0
-count = 0
+# # Reverse Num + Palindrome + Armstrong...
+# revNum = 0
+# count = 0
+# sum = 0
 
 n = int(input())
-dup = n
-while(n>0):
-    lastDigit = n % 10
-    count = count + 1
-    n = n // 10
-    revNum = revNum * 10 + lastDigit
-if(dup == revNum):
-    print("Is no. is palindrome:- Yes")
-else:
-    print("Is no. is palindrome:- No")
+# dup = n
+# while(n>0):
+#     lastDigit = n % 10
+#     count = count + 1
+#     n = n // 10
+#     revNum = revNum * 10 + lastDigit
+#     sum = sum + (lastDigit * lastDigit * lastDigit)
+# if(dup == revNum):
+#     print("Is no. is palindrome:- Yes")
+# else:
+#     print("Is no. is palindrome:- No")
 
-print("No.", count)
-print("RevN", revNum)
+# if(dup == sum):
+#     print("Is no. is armstrong:- Yes")
+# else:
+#     print("Is no. is armstrong:- No")
+
+# print("No.", count)
+# print("RevN", revNum)
+
+
+# print All Divisions
+import math
+divisors = []
+for i in range(1, int(math.sqrt(n)) + 1):
+    if n%i == 0:
+        divisors.append(i)
+        if n // i != i:
+            divisors.append(n//i)
+divisors.sort()
+print(f"Numbers Divided by {n} :- {divisors}")
+
