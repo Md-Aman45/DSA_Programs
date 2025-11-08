98acbc17c3c32a777bcb6aa5f1ef6b6d559f7c13
Md Aman <aman003245@gmail.com>
2025-11-08 15:26:43 +0530

    day26

--- java/Arrays/Day26/ReversePairs.java
+++ java/Arrays/Day26/ReversePairs.java
@@ -32,6 +32,76 @@ public class ReversePairs {
 
 
 
-        
+        // -------- OPTIMAL APPROACH -----------
+        System.out.print("Reverse Pairs: " + mergeSort(arr, 0, n - 1));
+
+        sc.close();
+    }
+
+
+
+    // -------- OPTIMAL APPROACH -----------
+    private static void merge(int[] arr, int low, int mid, int high) {
+        List<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            }
+            else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+
+
+
+    private static int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && (long) arr[i] > 2L * arr[right]) right++;
+            cnt += (right - (mid + 1));
+        }
+
+        return cnt;
+    }
+
+
+
+
+
+    private static int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+
+        int mid = (low + high) / 2;
+        cnt += mergeSort(arr, low, mid);
+        cnt += mergeSort(arr, mid + 1, high);
+        cnt += countPairs(arr, low, mid, high);
+        merge(arr, low, mid, high);
+
+        return cnt;
+
     }
 }
--- python/Arrays/day26/reversePairs.py
+++ python/Arrays/day26/reversePairs.py
@@ -7,7 +7,8 @@ def merge_sort(arr, low, high):
     mid = (low + high) // 2
     cnt += merge_sort(arr, low, mid)
     cnt += merge_sort(arr, mid + 1, high)
-    cnt += merge(arr, low, mid, high)
+    cnt += count_pairs(arr, low, mid, high)
+    merge(arr, low, mid, high)
 
     return cnt
 
@@ -17,7 +18,6 @@ def merge(arr, low, mid, high):
     temp = []
     left = low
     right = mid + 1
-    cnt = 0
 
     while left <= mid and right <= high:
         if arr[left] <= arr[right]:
@@ -26,7 +26,6 @@ def merge(arr, low, mid, high):
         
         else:
             temp.append(arr[right])
-            cnt += (mid - left + 1)
             right += 1
     
     while left <= mid:
@@ -39,6 +38,18 @@ def merge(arr, low, mid, high):
 
     for i in range(low, high + 1):
         arr[i] = temp[i - low]
+
+
+
+
+def count_pairs(arr, low, mid, high):
+    right = mid + 1
+    cnt = 0
+    for i in range(low, mid + 1):
+        while right <= high and arr[i] > 2 * arr[right]:
+            right += 1
+        
+        cnt += (right - (mid + 1))
     
     return cnt
 
@@ -48,21 +59,21 @@ def merge(arr, low, mid, high):
 
 
 # -------- MAIN --------
-n = int(input("Enter number of elements: "))
+n = int(input("Enter number of element: "))
 
 arr = list(map(int, input(f"Enter {n} numbers: ").split()))
 
 
 # -------- OPTIMAL APPROACH --------
-print("Inversion Count:", merge_sort(arr, 0, n - 1))
+print("Reverse Pairs:", merge_sort(arr, 0, n - 1))
 
 
 
-# -------- BRUTE FORCE APPROACH ---------
+# # -------- BRUTE FORCE APPROACH ---------
 # cnt = 0
 # for i in range(n - 1):
 #     for j in range(i + 1, n):
-#         if arr[i] > arr[j]:
+#         if arr[i] > 2 * arr[j]:
 #             cnt += 1
-        
-# print("Inversion Count:", cnt)
+    
+# print("Reverse Pairs:", cnt)
