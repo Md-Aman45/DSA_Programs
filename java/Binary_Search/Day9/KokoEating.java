6a7e2a1cc3745af85cb610ce0220e4ac58e4a355
Md Aman <aman003245@gmail.com>
2025-11-26 18:47:16 +0530

    java

---
+++ java/Binary_Search/Day9/KokoEating.java
@@ -0,0 +1,75 @@
+package Day9;
+import java.util.*;
+
+public class KokoEating {
+
+    // Function to find max element in array...
+    public int findMax(int[] arr, int n) {
+        int maxi = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        return maxi;
+    }
+
+    // Function to calculate total hours needed at speed k...
+    public int calculateHours(int[] piles, int k) {
+        int hours = 0;
+
+        for (int pile : piles) {
+            hours += (pile + k - 1) / k;     // same as ceil(pile / k)...
+        }
+
+        return hours;
+    }
+
+
+
+    // Main function to find minimum eating speed...
+    public int minEatingSpeed(int[] piles, int h, int n) {
+        int low = 1, high = findMax(piles, n);
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int totalhrs = calculateHours(piles, mid);
+
+            if (totalhrs <= h) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of piles: ");
+        int n = sc.nextInt();
+
+        int[] piles = new int[n];
+        System.out.println("Enter pile sizes:");
+        for (int i = 0; i < n; i++) {
+            piles[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter total hours H: ");
+        int h = sc.nextInt();
+
+        KokoEating k = new KokoEating();
+        int ans = k.minEatingSpeed(piles, h, n);
+
+        System.out.println("Minimum eating speed: " + ans + " bananas/hour");
+
+
+        sc.close();
+    }
+}
