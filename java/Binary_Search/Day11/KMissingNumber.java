bbb69b672ec1547c1f9f7f95d317e5ac4114ff79
Md Aman <aman003245@gmail.com>
2025-11-28 18:35:16 +0530

    java

---
+++ java/Binary_Search/Day11/KMissingNumber.java
@@ -0,0 +1,52 @@
+package Day11;
+import java.util.*;
+
+public class KMissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the no. of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter " + n + " " + "numbers: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+
+        KMissingNumber obj = new KMissingNumber();
+        int ans = obj.findKthMissing(arr, k);
+
+        System.out.print("Output: " + ans);
+
+
+        sc.close();
+
+
+    }
+
+    public int findKthMissing(int[] arr, int k) {
+        int n = arr.length;
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) low = mid + 1;
+
+            else high = mid - 1;
+        }
+
+        return (high + 1 + k);
+        // return low + k;
+    }
+}
