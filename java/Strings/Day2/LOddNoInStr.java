07d40e7f853b34de5787038b660a82ed4601a7a7
Md Aman <aman003245@gmail.com>
2025-12-08 13:37:22 +0530

    java

---
+++ java/Strings/Day2/LOddNoInStr.java
@@ -0,0 +1,31 @@
+package Day2;
+import java.util.*;
+
+public class LOddNoInStr {
+    public String LargestOddNumInStr(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            char ch = num.charAt(i);
+
+            if ((ch - '0') % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter numeric string: ");
+        String input = sc.nextLine();
+
+        LOddNoInStr obj = new LOddNoInStr();
+
+        String ans = obj.LargestOddNumInStr(input);
+
+        System.out.println("Largest odd number in String: " + ans);
+
+        sc.close();
+    }
+}
