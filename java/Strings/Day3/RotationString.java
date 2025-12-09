4a831e159c83c5208772e26d626ed518fc7f243b
Md Aman <aman003245@gmail.com>
2025-12-09 20:46:51 +0530

    java

---
+++ java/Strings/Day3/RotationString.java
@@ -0,0 +1,34 @@
+package Day3;
+import java.util.*;
+
+public class RotationString {
+    public boolean areRotations(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        String concat = s1 + s1;
+        
+        return concat.contains(s2);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String s1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String s2 = sc.nextLine();
+
+        RotationString obj = new RotationString();
+        boolean result = obj.areRotations(s1, s2);
+
+        if (result) {
+            System.out.println("Result: The strings ARE rotations of each other.");
+        } else {
+            System.out.println("Result: The strings are NOT rotations of each other.");
+        }
+
+        sc.close();
+    }
+}
