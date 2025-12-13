36bf79815e283f4e1c3a817d345d5ec561db6432
Md Aman <aman003245@gmail.com>
2025-12-13 15:52:24 +0530

    java

---
+++ java/Strings/Day7/LongestPalindrome.java
@@ -0,0 +1,53 @@
+package Day7;
+import java.util.*;
+
+public class LongestPalindrome {
+    public String longestPalindrome(String str) {
+        if (str == null || str.length() <= 1) return str;
+
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            int low = i, high = i;
+
+            while (low >= 0 && high < str.length() && str.charAt(low) == str.charAt(high)) {
+                if (high - low + 1 > maxLen)  {
+                    start = low;
+                    maxLen = high - low + 1;
+                }
+                low--;
+                high++;
+            }
+
+            low = i;
+            high = i + 1;
+
+            while (low >= 0 && high < str.length() && str.charAt(low) == str.charAt(high)) {
+                if (high - low + 1 > maxLen) {
+                    start = low;
+                    maxLen = high - low + 1;
+                }
+                low--;
+                high++;
+            }
+        }
+        return str.substring(start, start + maxLen);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        LongestPalindrome obj = new LongestPalindrome();
+        String result = obj.longestPalindrome(input);
+
+        System.out.println("Longest Palindromic Substring: " + result);
+
+        sc.close();
+    }
+}
