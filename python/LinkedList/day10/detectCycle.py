a789f3369e5fb3d021c4b715436006da071ed867
Md Aman <aman003245@gmail.com>
2025-12-29 15:07:56 +0530

    day 10

---
+++ python/LinkedList/day10/detectCycle.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.back = None
+
+
+def detectLoop(head):
+    # brute approach...
+    visited = set()
+    temp = head
+
+    while temp is not None:
+        if temp in visited:
+            return True
+
+        visited.add(temp)
+
+        temp = temp.next
+    
+    return False
+
+
+
+
+
+
+
+
+# ----------- MAIN ------------
+n = int(input("Enter number of nodes: "))
+print("Enter node values:", end=" ")
+
+values = list(map(int, input().split()))
+
+head = None
+prev = None
+
+for val in values:
+    node = Node(val)
+    if head is None:
+        head = node
+    else:
+        prev.next = node
+    prev = node
+
+# Create cycle (optional)
+pos = int(input("Enter position to create cycle (0 for no cycle): "))
+
+if pos > 0:
+    temp = head
+    for _ in range(pos - 1):
+        temp = temp.next
+    prev.next = temp
+
+# Detect cycle
+if detectLoop(head):
+    print("Cycle detected")
+else:
+    print("No cycle detected")
