f7c1b6da0d66a781cb51504e7d0dadb7a9f5e627
Md Aman <aman003245@gmail.com>
2026-01-10 18:44:52 +0530

    java

--- java/recursions_backtrackings/Day2/Para_Func.java
+++ java/recursions_backtrackings/Day2/Para_Func.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Para_Func {
 
     /*
-     * // ---------- Parameterused ----------
+     * // ---------- Parameterised ----------
      * // sum of first n numbers...
      * public static void sumOfN(int i, int sum) {
      * if (i < 1) {
@@ -32,6 +32,8 @@ public class Para_Func {
 
     
 
+
+/* 
     // ----------- Funtional -----------
     // sum of first n numbers...
     public static int sumOfN(int n) {
@@ -50,6 +52,64 @@ public class Para_Func {
         int result = sumOfN(n);
 
         System.out.println("result = " + result);
+
+
+        sc.close();
+    }
+*/
+
+
+
+
+
+/*
+    // ---------- Parameterised ----------
+    // Factorial of n numbers...
+    public static void factOfN(int i, int mul) {
+        if (i < 1) {
+            System.out.println(mul);
+            return;
+        }
+
+        factOfN(i - 1, mul * i);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n = ");
+        int n = sc.nextInt();
+
+        factOfN(n, 1);
+
+        sc.close();
+    }
+*/
+
+
+
+
+    // ----------- Funtional -----------
+    // Factorial of n numbers...
+    public static int factOfN(int n) {
+        if (n == 0) return 1;
+
+        return n * factOfN(n - 1);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n = ");
+
+        int n = sc.nextInt();
+
+        int result = factOfN(n);
+
+        System.out.println("result = " + result);
+
     }
 
 }
