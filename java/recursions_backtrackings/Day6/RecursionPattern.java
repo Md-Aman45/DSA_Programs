e866dc122f6baebf767fc21db692eb10ce7f63dd
Md Aman <aman003245@gmail.com>
2026-01-22 15:00:32 +0530

    java

---
+++ java/recursions_backtrackings/Day6/RecursionPattern.java
@@ -0,0 +1,49 @@
+package recursions_backtrackings.Day6;
+import java.util.*;
+
+public class RecursionPattern {
+    public static void printSubsequence(int i, int[] arr, ArrayList<Integer> ds, int s, int sum) {
+        if (i == arr.length) {
+            if (s == sum) {
+                System.out.println(ds);
+            }
+            return;
+        }
+
+
+        // pick the element...
+        ds.add(arr[i]);
+        s += arr[i];
+        printSubsequence(i + 1, arr, ds, s, sum);
+
+        // not pick the element...
+        ds.remove(ds.size() - 1);
+        s -= arr[i];
+        printSubsequence(i + 1, arr, ds, s, sum);
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of arr: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target sum K: ");
+        int k = sc.nextInt();
+
+        ArrayList<Integer> ds = new ArrayList<>();
+
+        printSubsequence(0, arr, ds, 0, k);
+    }
+}
